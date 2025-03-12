ecbb61525a1c830f5ef36f1e0d7da2c966369089
Shahriyor <baxtiyarovich0102@gmail.com>
2025-03-12 14:28:13 +0500

    Tugatildi

--- models/post.model.js
+++ models/post.model.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const postSchema = new mongoose.Schema({
   title: { type: String },
   content: { type: String },
-  author: { type: String },
+  author: { type: mongoose.Types.ObjectId, ref: "users" },
   image: { type: String, required: false },
   createdAt: { type: Date, default: Date.now },
 });
